fd968d2b31390283d4f581239120e3f22179f3ad
BannyT <tumusiimebarnabus@gmail.com>
2026-06-26 15:18:02 +0300

    upadtes

---
+++ src/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+    // javascript lives here
+     
+  return (
+    <div className='header'>
+        <div className='container headercontainer'>
+           <div className='logo'>
+                  <span className='logotext'>StratCom Ug</span>
+           </div>
+
+        </div>
+       
+    </div>
+  )
+}
+
+export default Header
