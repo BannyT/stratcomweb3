2de8f0950359aaa22e2515ecb9b5a27a9fcdbaf3
BannyT <tumusiimebarnabus@gmail.com>
2026-07-02 14:48:10 +0300

    updates

--- src/Header.js
+++ src/Header.js
@@ -3,14 +3,21 @@ import './Header.css'
 
 function Header() {
     // javascript lives here
-     
+
+    
   return (
     <div className='header'>
         <div className='container headercontainer'>
            <div className='logo'>
                   <span className='logotext'>StratCom Ug</span>
            </div>
+           <nav className='nav-menu'>
+              <ul className='nav-list'>
+                 <li className='nav-itme'> <a href='/' >Home</a> </li>
 
+              </ul>
+
+           </nav>
         </div>
        
     </div>
