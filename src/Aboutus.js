af53231acf1653104eddc3a2899012fc1310d2ff
BannyT <tumusiimebarnabus@gmail.com>
2026-07-02 15:35:44 +0300

    updtes data

---
+++ src/Aboutus.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Aboutus.css'
+
+function Aboutus() {
+  return (
+    <div className='about-section'>
+         <div className='about-container'>
+              <div className='about-header'>
+                   <span className='about-badge'>Our Story</span>
+                   <h2 className='about-title'> About Stratcom</h2>
+                   <p className='about-subtitle'> Empowering the next generation of Tech Programmers</p>
+              </div>
+         </div>
+    </div>
+  )
+}
+
+export default Aboutus
--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
 import Home from './Home';
+import Aboutus from './Aboutus';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Aboutus/>
       <Home/>
     </div>
   );
