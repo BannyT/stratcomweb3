c11675d2d3eab172688b749f7798155f6aaa281a
BannyT <tumusiimebarnabus@gmail.com>
2026-07-03 14:41:45 +0300

    updates dadat

--- src/Aboutus.js
+++ src/Aboutus.js
@@ -30,11 +30,27 @@ function Aboutus() {
                         <h3>Who We are</h3>
                         <p>Stratcom is a full-service strategic communications and marketing company dedicated to helping businesses, organizations, and institutions build meaningful connections with their audiences. We specialize in branding, public relations, digital marketing, corporate communications, media relations, content creation, and reputation management. By combining creative storytelling with data-driven strategies, we develop tailored communication solutions that enhance brand visibility, strengthen stakeholder engagement, and drive sustainable growth. Whether launching a new brand, managing corporate communications, or executing impactful marketing campaigns, Stratcom partners with clients to deliver measurable results and lastin</p>
                         <p>Mission Statement
-
 To empower organizations with innovative strategic communication, branding, and marketing solutions that inspire trust, foster meaningful engagement, and drive sustainable growth through creativity, integrity, and measurable impact.</p>
+                       <div className='mission-vision'>
+                          <div className='mission'>
+                              <h4>Our Mission</h4>
+                              <p>Democratize Tech Education and create Job ready Professionals</p>
+
+                          </div>
+                          <div className='vision'>
+                              <h4>Our Vision</h4>
+                              <p>To be Africa's Leading Tech Training eco System, recognized globally for Excellence and Innovation</p>
+
+                          </div>
+
+                       </div>
                   </div>
 
               </div>
+               <div className='about-image'>
+                    <img src='https://plus.unsplash.com/premium_photo-1661877737564-3dfd7282efcb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fHRlY2h8ZW58MHx8MHx8fDA%3D'/>
+
+               </div>
          </div>
     </div>
   )
