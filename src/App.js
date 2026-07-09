615cc8d471b08467ba9f2862b9dfb1b901899b31
BannyT <tumusiimebarnabus@gmail.com>
2026-07-09 14:12:54 +0300

    updates data

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import Header from './Header';
 import Home from './Home';
 import Aboutus from './Aboutus';
+import Services from './Services';
+import  {BrowserRouter as Router,Routes,Route} from  'react-router-dom'
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Aboutus/>
-      <Home/>
+      <Services/>
     </div>
   );
 }
---
+++ src/Signup.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Signup.css'
+
+function Signup() {
+  return (
+    <div>
+      <h2>SignUp is here</h2>
+    </div>
+  )
+}
+
+export default Signup
