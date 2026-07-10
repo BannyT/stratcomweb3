8abf4c730cc64ffc9a7e7eaf3d2e68c4849e0d3f
BannyT <tumusiimebarnabus@gmail.com>
2026-07-10 14:20:37 +0300

    upaders

--- src/Login.js
+++ src/Login.js
@@ -7,8 +7,18 @@ function Login() {
         <div className='login-card'>
            <div className='login-header'>
                  <span className='login-brand'> Stratcom</span>
-                 <h2>Wecome Back</h2>
+                 <h2>Welcome Back</h2>
                  <p> Sign In to Access Your Account</p>
+           </div>
+           <div className='login-form'>
+              <div className='login-form-group'>
+                    <label>Email Address</label>
+                    <input type='text' placeholder="e.g tom@gmail.com" />
+              </div>
+              <div className='login-form-group'>
+                    <label>Password</label>
+                    <input type='password' placeholder="*******" />
+              </div>
            </div>
 
         </div>
